fac816021043572133588732ce3a69337a4148e2
nirrie <97035721@st.deltion.nl>
2025-02-14 11:59:41 +0100

    created button component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { Link } from 'expo-router'
-import { Image } from 'expo-image';
+import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
