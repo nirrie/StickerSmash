574fab45e5386db8fd10c72ba1ef940e209ff538
nirrie <97035721@st.deltion.nl>
2025-02-14 11:24:12 +0100

    changed layout of navigationbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,41 @@
 import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Tablayout() {
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{ title: 'Home' }} />
-            <Tabs.Screen name="about" options={{ title: 'About' }} />
+        <Tabs
+        screenOptions={{ 
+            tabBarActiveTintColor: '#ffd33d',
+            headerStyle: {
+                backgroundColor: '#25292e',
+            },
+            headerShadowVisible: false,
+            headerTintColor: '#fff',
+            tabBarStyle: {
+                backgroundColor: '#25292e',
+            },
+        }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} size={24} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="about"
+                options={{
+                    title: 'About',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? 'information-circle-sharp' : 'information-circle-outline'} size={24} color={color} />
+                    ),
+                }}
+            />
         </Tabs>
     );
 }
+
+        
