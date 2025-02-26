daa724f33a658d53748becf9215024f9593a7e03
nirrie <97035721@st.deltion.nl>
2025-02-26 09:44:19 +0100

    new state variable called selectedImage using usestate hook. used to hold the URI of selected img, updated pickImageSync

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,12 @@ import { View, StyleSheet } from 'react-native';
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
 import * as ImagePicker from 'expo-image-picker';
-import { Link } from 'expo-router';
-import { Image } from 'expo-image';
-
+import { useState } from 'react';
 
 const PlaceholderImage = require('@/assets/images/images/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] =useState<string | undefined>(undefined);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,7 +17,7 @@ export default function Index() {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select any image');
     }
@@ -27,7 +26,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
