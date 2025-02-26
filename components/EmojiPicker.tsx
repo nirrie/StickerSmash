4c8a54ca0b1536958bf27b0345a2efc3ae03cd96
nirrie <97035721@st.deltion.nl>
2025-02-26 14:39:01 +0100

    fix typo props = PropsWithChildren

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -2,10 +2,10 @@ import { Modal, View, Text, Pressable, StyleSheet } from 'react-native';
 import { PropsWithChildren } from 'react';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-type Props = {
+type Props = PropsWithChildren<{
     isVisible: boolean;
     onClose: () => void;
-};
+}>;
 
 export default function EmojiPicker({ isVisible, children, onClose }: Props) {
     return (
