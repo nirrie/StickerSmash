ebcce7b95af107e2186f5d4b3f71480ecc526f78
nirrie <97035721@st.deltion.nl>
2025-02-28 15:47:40 +0100

    import gesture, animatedStyle, useSharedValue to emojiSticker.tsx

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,12 +1,16 @@
 import { View } from 'react-native';
 import { Image, type ImageSource } from 'expo-image';
 
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
 type Props = {
     imageSize: number;
     stickerSource: ImageSource;
 };
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+    const scaleImage = useSharedValue(imageSize);
     return (
         <View style={{ top: -350 }}>
             <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
