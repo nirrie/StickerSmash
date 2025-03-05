c70037078e7fd5871e6f2550f648c2604050ad79
nirrie <97035721@st.deltion.nl>
2025-03-05 10:55:03 +0100

    add tap gesture creating doubleTap object, use Gesture.Tap to animate/scaling sticker

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -11,6 +11,16 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     const scaleImage = useSharedValue(imageSize);
+    const doubleTap = Gesture.Tap()
+        .numberOfTaps(2)
+        .onStart(() => {
+        if (scaleImage.value === imageSize * 2) {
+            scaleImage.value = scaleImage.value;
+        } else {
+            scaleImage.value = Math.round(scaleImage.value / 2);
+        }
+        });
+    
     return (
         <View style={{ top: -350 }}>
             <Animated.Image
