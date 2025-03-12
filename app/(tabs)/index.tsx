c6db6699ece0239d8e87815eb7bd0ea12e65079d
nirrie <97035721@st.deltion.nl>
2025-03-12 14:13:27 +0100

    update onSaveImageAsync( if web it uses domtoimage to convert view to jpeg image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,20 @@
-import { View, StyleSheet } from 'react-native';
-import Button from '@/components/Button';
-import ImageViewer from '@/components/ImageViewer';
+import { View, StyleSheet, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useState, useRef } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { type ImageSource } from 'expo-image';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
+import ImageViewer from '@/components/ImageViewer';
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
+import Button from '@/components/Button';
 import EmojiPicker from '@/components/EmojiPicker'; 
 import EmojiList from '@/components/EmojiList';
 import EmojiSticker from '@/components/EmojiSticker';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 
 const PlaceholderImage = require('@/assets/images/images/background-image.png');
 
@@ -56,6 +58,7 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
     try {
       const localUri = await captureRef(imageRef, {
         height: 440,
@@ -68,6 +71,23 @@ export default function Index() {
       }
     } catch (error) {
       console.log(error);
+      } 
+    } else {
+      try {
+        // @ts-ignore
+        const dataUrl = await domtoimage.toJpeg(imageRef.current , {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
